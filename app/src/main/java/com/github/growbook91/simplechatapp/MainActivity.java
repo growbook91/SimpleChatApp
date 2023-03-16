cb63620e0a424dabe30a3c61c800a96dc759d089
growbook91 <70497021+growbook91@users.noreply.github.com>
2023-03-16 11:44:46 +0900

    Bottom Navigation Bar is adapted.

--- app/src/main/java/com/github/growbook91/simplechatapp/MainActivity.java
+++ app/src/main/java/com/github/growbook91/simplechatapp/MainActivity.java
@@ -1,14 +1,51 @@
 package com.github.growbook91.simplechatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private MainMenuChatFragment fragmentChat = new MainMenuChatFragment();
+    private MainMenuFriendFragment fragmentFriend = new MainMenuFriendFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.menu_frame_layout, fragmentChat).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new ItemSelectedListener());
+
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (menuItem.getItemId()) {
+                case R.id.menu_chat:
+                    transaction.replace(R.id.menu_frame_layout, fragmentChat).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_friend:
+                    transaction.replace(R.id.menu_frame_layout, fragmentFriend).commitAllowingStateLoss();
+                    break;
+
+            }
+
+            return true;
+        }
+
     }
 }
---
+++ app/src/main/java/com/github/growbook91/simplechatapp/MainMenuFriendFragment.java
@@ -0,0 +1,20 @@
+package com.github.growbook91.simplechatapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainMenuFriendFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main_menu_friend, container, false);
+
+        return rootView;
+    }
+}
